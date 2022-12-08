d43e5a0698a35f5e5be69cf8f22712673ec81d4f
tejasatreya <32978758+tejasatreya@users.noreply.github.com>
2022-12-08 12:31:39 -0500

    Add files via upload

---
+++ EpochBasedReclamation/Node.h
@@ -0,0 +1,13 @@
+#pragma once;
+#include<atomic>
+#include<iostream>
+class Node {
+    public:
+        int data;
+        std::atomic<Node*> nextNode;
+        int retire;                                          // Retire epoch variable
+        Node(int data){
+            this->data = data;
+            nextNode.store(NULL, std::memory_order_relaxed); //Since we need atomicity, we use relaxed.
+        }
+};
